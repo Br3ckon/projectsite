dee784f72a0acc42e8edfbdb34f42cc14c99c68b
Br3ckon <122642174+Br3ckon@users.noreply.github.com>
2023-01-23 22:48:18 +0000

    Add files via upload
    sorted the hamburger out

--- index.js
+++ index.js
@@ -2,15 +2,28 @@ const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 // const navLink = document.querySelector(".nav-menu")
 
+const navitems = document.querySelector(".nav-menu")
+
 hamburger.addEventListener("click", () => {
+
+   
     hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+    navMenu.classList.toggle("active")
+
 
 })
 
 
-// navLink.addEventListener("click",  {
+
+// navitem.addEventListener("click",  {
  //   hamburger.classList.toggle("active");
 //navMenu.classList.toggle("closed");
 
 //}) 
+
+navitems.addEventListener("click", function() {
+    navMenu.classList.toggle("active")
+
+
+
+})
